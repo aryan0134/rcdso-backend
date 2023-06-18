2e6b38bf470e755cad39b1da5139b4f30e6255b8
Aryan Sindhu <81367645+aryan0134@users.noreply.github.com>
2023-06-18 16:46:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,18 @@ const bodyParser = require('body-parser');
 const app=express()
 const db = process.env.DATABASE
 const port = process.env.PORT
-app.set('env', process.env.NODE_ENV || 'development');
-app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
 
 //Middleware
 
