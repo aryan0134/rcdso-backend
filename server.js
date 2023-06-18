8a74c4f7e75c2391c12f38544cb4d2b776bcef27
Aryan Sindhu <81367645+aryan0134@users.noreply.github.com>
2023-06-18 20:27:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const path = require('path')
 const app=express()
 const db = process.env.DATABASE
 const port = process.env.PORT
-// app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 
@@ -85,16 +85,16 @@ app.use(
 	})
 );
 
-// app.all('*', (req, res) => {
-//     res.status(404)
-//     if (req.accepts('html')) {
-//         res.sendFile(path.join(__dirname, 'views', '404.html'))
-//     } else if (req.accepts('json')) {
-//         res.json({ message: '404 Not Found' })
-//     } else {
-//         res.type('txt').send('404 Not Found')
-//     }
-// })
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
 
 //connecting to MongoDB
 
