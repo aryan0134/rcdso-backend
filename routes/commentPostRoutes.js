011539c0d5c7a43a1f10f631f6aa5161ed397a08
Aryan Sindhu <81367645+aryan0134@users.noreply.github.com>
2023-06-24 13:22:07 +0530

    Add files via upload

---
+++ routes/commentPostRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const {createComments} = require('../controllers/commentControllers')
+const requireAuth = require('../middleware/requireAuth')
+
+const router = express.Router()
+
+// //Require Auth For All comments
+router.use(requireAuth)
+
+
+//POST a new comment
+router.post('/' , createComments )
+
+
+
+
+
+module.exports = router
