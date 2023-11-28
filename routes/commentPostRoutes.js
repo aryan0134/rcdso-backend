4b1988e74fe79fbfefc084acfa1d1bb31bad96fe
Aryan Sindhu <81367645+aryan0134@users.noreply.github.com>
2023-11-28 14:32:05 +0530

    Update commentPostRoutes.js

--- routes/commentPostRoutes.js
+++ routes/commentPostRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const {createComments} = require('../controllers/commentControllers')
-const requireAuth = require('../middleware/requireAuth')
+//const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
 
 // //Require Auth For All comments
-router.use(requireAuth)
+// router.use(requireAuth)
 
 
 //POST a new comment
